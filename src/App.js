22a0560e2309500db381ab703bd8b1d53f181e00
Aartisoni3515 <aartisoni3517@gmail.com>
2022-12-27 12:36:54 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
 // import Calculator from "./component/Calculator";
-// import Weather from "./component/Weather";
+import Weather from "./component/Weather";
 // import Form from "./component/Form";
-import NewPro from "./component/NewPro";
+// import NewPro from "./component/NewPro";
 
 function App() {
 
   return (
     <>
       {/* <Calculator/> */}
-      {/* <Weather/> */}
+      <Weather/>
       {/* <Form/> */}
-      <NewPro/>
+      {/* <NewPro/> */}
     </>
   );
 }
